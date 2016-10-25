64cb63fc1977c73e7c06283df6e8bd7c9972380b
Reza Afzalan <rafzalan@gmail.com>
2016-10-25 15:48:12 +0330

    Update coolproptest.jl

--- test/coolproptest.jl
+++ test/coolproptest.jl
@@ -1,20 +1,22 @@
 using CoolProp
 #CoolProp
-println("testing CoolProp....")
-#PropsSI
-@test_approx_eq 373.1242958476879 PropsSI("T","P",101325.0,"Q",0.0,"Water")
-#PhaseSI
-#get_global_param_string
-#get_parameter_information_string
-#get_fluid_param_string
-#set_reference_stateS
-#get_param_index
-#get_input_pair_index
-#F2K
-#K2F
-#HAPropsSI
-#AbstractState_factory
-#AbstractState_free
-#AbstractState_set_fractions
-#AbstractState_update
-#AbstractState_keyed_output
+@static is_apple() ? println("CoolProp: no osx support") : begin
+  println("testing CoolProp....")
+  #PropsSI
+  @test_approx_eq 373.1242958476879 PropsSI("T","P",101325.0,"Q",0.0,"Water")
+  #PhaseSI
+  #get_global_param_string
+  #get_parameter_information_string
+  #get_fluid_param_string
+  #set_reference_stateS
+  #get_param_index
+  #get_input_pair_index
+  #F2K
+  #K2F
+  #HAPropsSI
+  #AbstractState_factory
+  #AbstractState_free
+  #AbstractState_set_fractions
+  #AbstractState_update
+  #AbstractState_keyed_output
+end
