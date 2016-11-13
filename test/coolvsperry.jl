f796bfbb43e5ea53e6f501c369a6cd48700d6bfa
reza afzalan <rafzalan@gmail.com>
2016-11-13 08:22:32 +0330

    test for coolprop params

--- test/coolvsperry.jl
+++ test/coolvsperry.jl
@@ -16,3 +16,14 @@ end
 for param in ["version", "gitrevision", "errstring", "warnstring", "FluidsList", "incompressible_list_pure", "incompressible_list_solution", "mixture_binary_pairs_list", "parameter_list", "predefined_mixtures", "HOME", "cubic_fluids_schema", "invalid"]
   println(param * " = " * get_global_param_string(param))
 end
+param_list = split(get_global_param_string("parameter_list"),',');
+fluids_list = split(get_global_param_string("FluidsList"),',');
+println("number of defined parameters = $(length(param_list))")
+for param in param_list
+  println("$param ---> $(get_parameter_information_string(String(param)))");
+end
+for fluid in fluids_list
+  for param in param_list
+    #println("$fluid $param $(get_fluid_param_string(String(fluid), String(param)))");
+  end
+end
